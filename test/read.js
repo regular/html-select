661b38ec6f807959fae3687ab57f43db9b99de3f
James Halliday <mail@substack.net>
2014-04-30 18:09:44 -0700

    passing outer read stream test

--- test/read.js
+++ test/read.js
@@ -21,3 +21,23 @@ test('read stream', function (t) {
     s.write([ 'close', '</html>' ]);
     s.end();
 });
+
+test('outer read stream', function (t) {
+    t.plan(2);
+    var s = select('div b', function (e) {
+        t.equal(e.name, 'b');
+        e.createReadStream({ outer: true }).pipe(concat(function (body) {
+            t.equal(body.toString('utf8'), '<b>beep boop</b>');
+        }));
+    });
+    s.write([ 'open', '<html>' ]);
+    s.write([ 'open', '<body>' ]);
+    s.write([ 'open', '<div>' ]);
+    s.write([ 'open', '<b>' ]);
+    s.write([ 'text', 'beep boop' ]);
+    s.write([ 'close', '</b>' ]);
+    s.write([ 'close', '<div>' ]);
+    s.write([ 'close', '</body>' ]);
+    s.write([ 'close', '</html>' ]);
+    s.end();
+});
