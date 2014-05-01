832fce9b3b9531794aae9e800faac6e4dc9fd3e0
James Halliday <mail@substack.net>
2014-04-30 18:10:38 -0700

    failing read stream case

--- test/read.js
+++ test/read.js
@@ -22,6 +22,29 @@ test('read stream', function (t) {
     s.end();
 });
 
+test('more read stream', function (t) {
+    t.plan(2);
+    var s = select('div b', function (e) {
+        t.equal(e.name, 'b');
+        e.createReadStream().pipe(concat(function (body) {
+            t.equal(body.toString('utf8'), 'beep boop');
+        }));
+    });
+    s.write([ 'open', '<html>' ]);
+    s.write([ 'open', '<body>' ]);
+    s.write([ 'open', '<div>' ]);
+    s.write([ 'text', 'whoa' ]);
+    s.write([ 'open', '</div>' ]);
+    s.write([ 'open', '<div>' ]);
+    s.write([ 'open', '<b>' ]);
+    s.write([ 'text', 'beep boop' ]);
+    s.write([ 'close', '</b>' ]);
+    s.write([ 'close', '<div>' ]);
+    s.write([ 'close', '</body>' ]);
+    s.write([ 'close', '</html>' ]);
+    s.end();
+});
+
 test('outer read stream', function (t) {
     t.plan(2);
     var s = select('div b', function (e) {
