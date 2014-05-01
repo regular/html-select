a57dc7065271a8f25c2caa60f7c6fcb2944b8b0d
James Halliday <mail@substack.net>
2014-04-30 18:14:58 -0700

    failing match test

---
+++ test/match.js
@@ -0,0 +1,23 @@
+var select = require('../');
+var test = require('tape');
+var concat = require('concat-stream');
+
+test('match once', function (t) {
+    t.plan(1);
+    var s = select('div b', function (e) {
+        t.equal(e.name, 'b');
+    });
+    s.write([ 'open', '<html>' ]);
+    s.write([ 'open', '<body>' ]);
+    s.write([ 'open', '<div>' ]);
+    s.write([ 'text', 'whoa' ]);
+    s.write([ 'open', '</div>' ]);
+    s.write([ 'open', '<div>' ]);
+    s.write([ 'open', '<b>' ]);
+    s.write([ 'text', 'beep boop' ]);
+    s.write([ 'close', '</b>' ]);
+    s.write([ 'close', '<div>' ]);
+    s.write([ 'close', '</body>' ]);
+    s.write([ 'close', '</html>' ]);
+    s.end();
+});
