9f09b12d7fa26444e1171bce5c69f97a4cf9c86c
James Halliday <mail@substack.net>
2014-04-30 18:09:04 -0700

    passing read stream test

---
+++ test/read.js
@@ -0,0 +1,23 @@
+var select = require('../');
+var test = require('tape');
+var concat = require('concat-stream');
+
+test('read stream', function (t) {
+    t.plan(2);
+    var s = select('div b', function (e) {
+        t.equal(e.name, 'b');
+        e.createReadStream().pipe(concat(function (body) {
+            t.equal(body.toString('utf8'), 'beep boop');
+        }));
+    });
+    s.write([ 'open', '<html>' ]);
+    s.write([ 'open', '<body>' ]);
+    s.write([ 'open', '<div>' ]);
+    s.write([ 'open', '<b>' ]);
+    s.write([ 'text', 'beep boop' ]);
+    s.write([ 'close', '</b>' ]);
+    s.write([ 'close', '<div>' ]);
+    s.write([ 'close', '</body>' ]);
+    s.write([ 'close', '</html>' ]);
+    s.end();
+});
