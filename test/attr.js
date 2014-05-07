b32609ca8ab0c4d0740a9a2d20cdc998720d32d0
James Halliday <mail@substack.net>
2014-05-06 23:17:42 -0700

    another mixed case test, passing

--- test/attr.js
+++ test/attr.js
@@ -45,3 +45,18 @@ test('mixed case attribute', function (t) {
     s.write([ 'close', '</html>' ]);
     s.end();
 });
+
+test('mixed case [attribute]', function (t) {
+    t.plan(2);
+    var s = select('input[TypE=text]', function (e) {
+        t.equal(e.name, 'input');
+        t.deepEqual(e.attributes, { type: 'text' });
+    });
+    s.write([ 'open', '<html>' ]);
+    s.write([ 'open', '<body>' ]);
+    s.write([ 'open', '<input type="submit">' ]);
+    s.write([ 'open', '<input type="text">' ]);
+    s.write([ 'close', '</body>' ]);
+    s.write([ 'close', '</html>' ]);
+    s.end();
+});
