27f567955f771c465303ca78a0a74df2a9dd2cf6
James Halliday <mail@substack.net>
2014-05-06 23:09:47 -0700

    failing mixed case test

--- test/attr.js
+++ test/attr.js
@@ -30,3 +30,18 @@ test('bare attribute', function (t) {
     s.write([ 'close', '</html>' ]);
     s.end();
 });
+
+test('mixed case attribute', function (t) {
+    t.plan(2);
+    var s = select('input[type=text]', function (e) {
+        t.equal(e.name, 'input');
+        t.deepEqual(e.attributes, { type: 'text' });
+    });
+    s.write([ 'open', '<html>' ]);
+    s.write([ 'open', '<body>' ]);
+    s.write([ 'open', '<input TyPe="submit">' ]);
+    s.write([ 'open', '<input tYpE="text">' ]);
+    s.write([ 'close', '</body>' ]);
+    s.write([ 'close', '</html>' ]);
+    s.end();
+});
