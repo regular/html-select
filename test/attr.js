9da6b6726a10e8fe75c4b01f159d7b9459d3cb2e
James Halliday <mail@substack.net>
2014-05-06 23:26:42 -0700

    failing test for attribute with trailing whitespace

--- test/attr.js
+++ test/attr.js
@@ -60,3 +60,18 @@ test('mixed case [attribute]', function (t) {
     s.write([ 'close', '</html>' ]);
     s.end();
 });
+
+test('trailing whitespace attribute', function (t) {
+    t.plan(2);
+    var s = select('input[TypE=text]', function (e) {
+        t.equal(e.name, 'input');
+        t.deepEqual(e.attributes, { type: 'text', value: 'xyz' });
+    });
+    s.write([ 'open', '<html>' ]);
+    s.write([ 'open', '<body>' ]);
+    s.write([ 'open', '<input type="submit">' ]);
+    s.write([ 'open', '<input type="text" value ="xyz">' ]);
+    s.write([ 'close', '</body>' ]);
+    s.write([ 'close', '</html>' ]);
+    s.end();
+});
