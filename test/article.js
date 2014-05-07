19a3749914865ff6e90088c3aba8f70652c83b45
James Halliday <mail@substack.net>
2014-05-06 20:54:30 -0700

    updated article test

--- test/article.js
+++ test/article.js
@@ -4,17 +4,54 @@ var tokenize = require('html-tokenize');
 var concat = require('concat-stream');
 var fs = require('fs');
 
+var names = [
+    'echojs',
+    'echojs',
+    'efcl',
+    'efcl',
+    'efcl',
+    'aneesha',
+    'echojs',
+    '紫云飞',
+    '紫云飞',
+    '紫云飞',
+    'carldanley',
+    'eskimoblood',
+    'pazguille',
+    'echojs',
+    'echojs',
+    '紫云飞',
+    'carldanley',
+    '紫云飞',
+    'echojs',
+    'eskimoblood',
+    '紫云飞',
+    '紫云飞',
+    '紫云飞',
+    'carldanley',
+    '紫云飞',
+    '紫云飞',
+    'echojs',
+    'echojs',
+    'echojs'
+];
+
 var expected = [];
-for (var i = 0; i < 32; i++) expected.push('echojs');
+for (var i = 0; i < names.length; i++) {
+    expected.push(names[i]);
+    expected.push('discuss');
+}
+expected.push('echojs');
+expected.push('1 comment');
 
 test('article', function (t) {
-    t.plan(2 * expected.length);
+    t.plan(expected.length * 2);
     fs.createReadStream(__dirname + '/article/index.html')
         .pipe(tokenize())
         .pipe(select('article username a[href]', function (e) {
             t.equal(e.name, 'a');
             e.createReadStream().pipe(concat(function (body) {
-                t.equal(body.toString(), expected.shift());
+                t.equal(body.toString(), expected.shift(), body+'');
             }));
         }))
     ;
