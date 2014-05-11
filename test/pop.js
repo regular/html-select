8e9468eaaa65b260ec6de478c5b5a1665390ac79
James Halliday <mail@substack.net>
2014-05-11 23:20:46 +0200

    failing test for more closing tags than opening

---
+++ test/pop.js
@@ -0,0 +1,24 @@
+var select = require('../');
+var test = require('tape');
+var concat = require('concat-stream');
+
+test('more closes than opens', function (t) {
+    t.plan(2);
+    var s = select('div b', function (e) {
+        t.equal(e.name, 'b');
+        e.createReadStream().pipe(concat(function (body) {
+            t.equal(body.toString('utf8'), 'beep boop');
+        }));
+    });
+    s.write([ 'open', '<html>' ]);
+    s.write([ 'open', '<body>' ]);
+    s.write([ 'open', '<div>' ]);
+    s.write([ 'open', '<b>' ]);
+    s.write([ 'text', 'beep boop' ]);
+    s.write([ 'close', '</b>' ]);
+    s.write([ 'close', '</div>' ]);
+    s.write([ 'close', '</quote>' ]);
+    s.write([ 'close', '</body>' ]);
+    s.write([ 'close', '</html>' ]);
+    s.end();
+});
