a54bbd0d2c69619ad113c0d9088209a358dc1ba2
James Halliday <mail@substack.net>
2014-05-17 10:02:01 +0200

    updated page example

--- example/page.js
+++ example/page.js
@@ -2,9 +2,11 @@ var select = require('../');
 var tokenize = require('html-tokenize');
 var fs = require('fs');
 
-fs.createReadStream(__dirname + '/page.html')
-    .pipe(tokenize())
-    .pipe(select('.content span', function (e) {
-        console.log('matched:', e);
-    }))
-;
+var s = select('ul > li dt', function (e) {
+    console.log('*** MATCH ***');
+    e.createReadStream().on('data', function (row) {
+        console.log([ row[0], row[1].toString() ]);
+    });
+});
+fs.createReadStream(__dirname + '/page.html').pipe(tokenize()).pipe(s);
+s.resume();
