63ed36e85ac8983be22da3f751a52415847f5bd6
James Halliday <mail@substack.net>
2014-05-17 10:02:17 +0200

    remove crufty examples

--- example/css.js
+++
@@ -1,9 +0,0 @@
-var cssauron = require('cssauron');
-var css = cssauron({
-    tag: function (node) {
-        console.log('LOOKUP TAG', node);
-    }
-});
-
-var sel = css('x');
-console.log(sel({ x : 3 }));
