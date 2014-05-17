e01336da2b2dab7d95197826f02f2d0fa1d0a3c0
James Halliday <mail@substack.net>
2014-05-17 08:25:20 +0200

    upgraded page test

--- test/page.js
+++ test/page.js
@@ -2,86 +2,135 @@ var select = require('../');
 var test = require('tape');
 var tokenize = require('html-tokenize');
 var fs = require('fs');
+var through = require('through2');
 
 test('page .content', function (t) {
-    t.plan(2);
-    fs.createReadStream(__dirname + '/page/index.html')
-        .pipe(tokenize())
-        .pipe(select('.content', function (e) {
-            t.equal(e.name, 'div');
-            t.deepEqual(e.attributes, { class: 'content' });
-        }))
-    ;
+    var expected = [
+        [ 'open', Buffer('<div class="content">') ],
+        [ 'text', Buffer('\n      ') ],
+        [ 'open', Buffer('<span class="greeting">') ],
+        [ 'text', Buffer('beep boop') ],
+        [ 'close', Buffer('</span>') ],
+        [ 'text', Buffer('\n      ') ],
+        [ 'open', Buffer('<span class="name">') ],
+        [ 'text', Buffer('robot') ],
+        [ 'close', Buffer('</span>') ],
+        [ 'text', Buffer('\n    ') ],
+        [ 'close', Buffer('</div>') ]
+    ];
+    t.plan(expected.length);
+    var s = select('.content', function (e) {
+        e.createReadStream().pipe(through.obj(function (row, enc, next) {
+            t.deepEqual(row, expected.shift());
+            next();
+        }));
+    });
+    readStream().pipe(tokenize()).pipe(s);
+    s.resume();
 });
 
 test('page *', function (t) {
-    t.plan(4);
     var expected = [
-        { name: 'span', attributes: { class: 'greeting' } },
-        { name: 'span', attributes: { class: 'name' } }
+        [ 'open', Buffer('<span class="greeting">') ],
+        [ 'text', Buffer('beep boop') ],
+        [ 'close', Buffer('</span>') ],
+        [ 'open', Buffer('<span class="name">') ],
+        [ 'text', Buffer('robot') ],
+        [ 'close', Buffer('</span>') ]
     ];
-    fs.createReadStream(__dirname + '/page/index.html')
-        .pipe(tokenize())
-        .pipe(select('.content *', function (e) {
-            var x = expected.shift();
-            t.equal(e.name, x.name);
-            t.deepEqual(e.attributes, x.attributes);
-        }))
-    ;
+    t.plan(expected.length);
+    var s = select('.content *', function (e) {
+        e.createReadStream().pipe(through.obj(function (row, enc, next) {
+            t.deepEqual(row, expected.shift());
+            next();
+        }));
+    });
+    readStream().pipe(tokenize()).pipe(s);
+    s.resume();
 });
 
 test('page div.content', function (t) {
-    t.plan(2);
-    fs.createReadStream(__dirname + '/page/index.html')
-        .pipe(tokenize())
-        .pipe(select('div.content', function (e) {
-            t.equal(e.name, 'div');
-            t.deepEqual(e.attributes, { class: 'content' });
-        }))
-    ;
+    var expected = [
+        [ 'open', Buffer('<div class="content">') ],
+        [ 'text', Buffer('\n      ') ],
+        [ 'open', Buffer('<span class="greeting">') ],
+        [ 'text', Buffer('beep boop') ],
+        [ 'close', Buffer('</span>') ],
+        [ 'text', Buffer('\n      ') ],
+        [ 'open', Buffer('<span class="name">') ],
+        [ 'text', Buffer('robot') ],
+        [ 'close', Buffer('</span>') ],
+        [ 'text', Buffer('\n    ') ],
+        [ 'close', Buffer('</div>') ]
+    ];
+    t.plan(expected.length);
+    var s = select('div.content', function (e) {
+        e.createReadStream().pipe(through.obj(function (row, enc, next) {
+            t.deepEqual(row, expected.shift());
+            next();
+        }));
+    });
+    readStream().pipe(tokenize()).pipe(s);
+    s.resume();
 });
 
 test('page .name', function (t) {
-    t.plan(4);
     var expected = [
-        { name: 'h1', attributes: { class: 'name' } },
-        { name: 'span', attributes: { class: 'name' } }
+        [ 'open', Buffer('<h1 class="name">') ],
+        [ 'text', Buffer('whoa') ],
+        [ 'close', Buffer('</h1>') ],
+        [ 'open', Buffer('<span class="name">') ],
+        [ 'text', Buffer('robot') ],
+        [ 'close', Buffer('</span>') ],
     ];
-    fs.createReadStream(__dirname + '/page/index.html')
-        .pipe(tokenize())
-        .pipe(select('.name', function (e) {
-            var x = expected.shift();
-            t.equal(e.name, x.name);
-            t.deepEqual(e.attributes, x.attributes);
-        }))
-    ;
+    t.plan(expected.length);
+    var s = select('.name', function (e) {
+        e.createReadStream().pipe(through.obj(function (row, enc, next) {
+            t.deepEqual(row, expected.shift());
+            next();
+        }));
+    });
+    readStream().pipe(tokenize()).pipe(s);
+    s.resume();
 });
 
-
 test('page span.greeting', function (t) {
-    t.plan(2);
-    fs.createReadStream(__dirname + '/page/index.html')
-        .pipe(tokenize())
-        .pipe(select('span.greeting', function (e) {
-            t.equal(e.name, 'span');
-            t.deepEqual(e.attributes, { class: 'greeting' });
-        }))
-    ;
+    var expected = [
+        [ 'open', Buffer('<span class="greeting">') ],
+        [ 'text', Buffer('beep boop') ],
+        [ 'close', Buffer('</span>') ],
+    ];
+    t.plan(expected.length);
+    var s = select('span.greeting', function (e) {
+        e.createReadStream().pipe(through.obj(function (row, enc, next) {
+            t.deepEqual(row, expected.shift());
+            next();
+        }));
+    });
+    readStream().pipe(tokenize()).pipe(s);
+    s.resume();
 });
 
 test('page .content span', function (t) {
-    t.plan(4);
-    
     var expected = [
-        { name: 'span', attributes: { class: 'greeting' } },
-        { name: 'span', attributes: { class: 'name' } }
+        [ 'open', Buffer('<span class="greeting">') ],
+        [ 'text', Buffer('beep boop') ],
+        [ 'close', Buffer('</span>') ],
+        [ 'open', Buffer('<span class="name">') ],
+        [ 'text', Buffer('robot') ],
+        [ 'close', Buffer('</span>') ]
     ];
-    fs.createReadStream(__dirname + '/page/index.html')
-        .pipe(tokenize())
-        .pipe(select('.content span', function (e) {
-            var x = expected.shift();
-            t.equal(e.name, x.name);
-            t.deepEqual(e.attributes, x.attributes);
-        }))
-    ;
+    t.plan(expected.length);
+    var s = select('.content span', function (e) {
+        e.createReadStream().pipe(through.obj(function (row, enc, next) {
+            t.deepEqual(row, expected.shift());
+            next();
+        }));
+    });
+    readStream().pipe(tokenize()).pipe(s);
+    s.resume();
 });
+
+function readStream () {
+    return fs.createReadStream(__dirname + '/page/index.html');
+}
