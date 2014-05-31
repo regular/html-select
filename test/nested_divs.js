8d7ddb91ab556da3dbbb6ca4dbd8bdba860be45a
James Halliday <mail@substack.net>
2014-05-31 07:51:21 -0700

    failing nested divs test

---
+++ test/nested_divs.js
@@ -0,0 +1,21 @@
+var select = require('../');
+var test = require('tape');
+var tokenize = require('html-tokenize');
+var through = require('through2');
+var fs = require('fs');
+
+test('nested divs', function (t) {
+    var s = select();
+    var expected = [
+        '<div    class = "a"  id="z"      >',
+        '<div class="b">'
+    ];
+    t.plan(expected.length);
+    s.select('div', function (e) {
+        t.equal(e._first[1].toString(), expected.shift());
+    });
+    fs.createReadStream(__dirname + '/nested_divs/index.html')
+        .pipe(tokenize()).pipe(s)
+    ;
+    s.resume();
+});
