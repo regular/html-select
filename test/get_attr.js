2bf9a9723ada910439f691b9868c9d58eb30533c
James Halliday <mail@substack.net>
2014-06-01 04:34:01 -0700

    another test for ordinary get attr

--- test/get_attr.js
+++ test/get_attr.js
@@ -2,7 +2,31 @@ var select = require('../');
 var test = require('tape');
 var through = require('through2');
 
-test('set an attribute', function (t) {
+test('get an attribute', function (t) {
+    var expected = [ 'a', 'b', 'c' ];
+    t.plan(expected.length);
+    
+    var s = select().select('div', function (e) {
+        t.equal(e.getAttribute('class'), expected.shift());
+    });
+    
+    s.write([ 'open', '<html>' ]);
+    s.write([ 'open', '<body>' ]);
+    s.write([ 'open', '<div class="a">' ]);
+    s.write([ 'open', '<div class="b">' ]);
+    s.write([ 'open', '<div class="c">' ]);
+    s.write([ 'text', 'beep boop' ]);
+    s.write([ 'close', '</div>' ]);
+    s.write([ 'close', '</div>' ]);
+    s.write([ 'close', '</div>' ]);
+    s.write([ 'close', '</body>' ]);
+    s.write([ 'close', '</html>' ]);
+    s.end();
+    
+    s.resume();
+});
+
+test('get a self-closing attribute', function (t) {
     var expected = [ 'submit', 'text' ];
     t.plan(expected.length);
     
