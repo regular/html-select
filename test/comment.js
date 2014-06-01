86ffeda7923ac12d2f92abd3f8bf280e634655a8
James Halliday <mail@substack.net>
2014-06-01 10:41:20 -0700

    failing self-closing comment test

--- test/comment.js
+++ test/comment.js
@@ -2,7 +2,7 @@ var select = require('../');
 var test = require('tape');
 var through = require('through2');
 
-test('read stream', function (t) {
+test('comment', function (t) {
     var expected = [
         [ 'text', 'stomping on this tiny house' ],
     ];
@@ -18,6 +18,30 @@ test('read stream', function (t) {
     s.write([ 'open', '<!-- beep boop -->' ]);
     s.write([ 'open', '<html>' ]);
     s.write([ 'text', 'stomping on this tiny house' ]);
+    s.write([ 'close', '</html>' ]);
+    s.end();
+    s.resume();
+});
+
+test('self-closing comment', function (t) {
+    var expected = [
+        [ 'text', 'stomping on this tiny house' ],
+    ];
+    t.plan(expected.length);
+    var s = select('b > span', function (e) {
+        e.createReadStream({ inner: true })
+            .pipe(through.obj(function (row, enc, next) {
+                t.deepEqual(row, expected.shift());
+                next();
+            }))
+        ;
+    });
+    s.write([ 'close', '<html>' ]);
+    s.write([ 'open', '<b>' ]);
+    s.write([ 'open', '<!-- eh -->' ]);
+    s.write([ 'open', '<span>' ]);
+    s.write([ 'text', 'stomping on this tiny house' ]);
+    s.write([ 'close', '</b>' ]);
     s.write([ 'close', '</html>' ]);
     s.end();
     s.resume();
