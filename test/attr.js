01b1017371a87d408dce8ddadba68e4108a963d7
James Halliday <mail@substack.net>
2014-06-02 15:26:03 -0700

    failing test for attribute values with spaces in them

--- test/attr.js
+++ test/attr.js
@@ -154,3 +154,29 @@ test('attribute whitespace around quotes', function (t) {
     s.end();
     s.resume();
 });
+
+test('exact attribute value with spaces', function (t) {
+    var expected = [
+        [ 'open', '<img class="a b c">' ]
+    ];
+    t.plan(expected.length);
+    
+    var s = select('img[class="a b c"]', function (e) {
+        e.createReadStream()
+            .pipe(through.obj(function (row, enc, next) {
+                t.deepEqual(row, expected.shift());
+                next();
+            }))
+        ;
+    });
+    s.write([ 'open', '<html>' ]);
+    s.write([ 'open', '<body>' ]);
+    s.write([ 'open', '<img class="a">' ]);
+    s.write([ 'open', '<img class="abc">' ]);
+    s.write([ 'open', '<img class="a b c d">' ]);
+    s.write([ 'open', '<img class="a b c">' ]);
+    s.write([ 'close', '</body>' ]);
+    s.write([ 'close', '</html>' ]);
+    s.end();
+    s.resume();
+});
