d45779cc238b2e84007d6831a08fb19528a0de85
James Halliday <mail@substack.net>
2014-07-18 01:17:05 -0700

    updated first test to use :first-child, still fails for a different reason

--- test/first.js
+++ test/first.js
@@ -9,15 +9,18 @@ test('first', function (t) {
     t.plan(2);
     var expected = [ 'AAA', 'DDD' ];
     var sel = select();
-    sel.select('.row *:first', function (elem) {
+    sel.select('.row *:first-child', function (elem) {
         var ex = expected.shift();
         var strip = through.obj(function (row, enc, next) {
             this.push(row[1]);
             next();
         });
-        elem.createReadStream().pipe(strip).pipe(concat(function (body) {
-            t.equal(body.toString('utf8'), ex);
-        }));
+        elem.createReadStream({ inner: true })
+            .pipe(strip)
+            .pipe(concat(function (body) {
+                t.equal(body.toString('utf8'), ex);
+            }))
+        ;
     });
     fs.createReadStream(__dirname + '/first/index.html')
         .pipe(tokenize())
